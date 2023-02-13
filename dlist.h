a78a4693e70ef050cb98690b66831478c97da526
Maya <mtrembla@student.42quebec.com>
2023-02-13 13:14:53 -0500

    Fixed remove_node

--- dlist.h
+++ dlist.h
@@ -6,7 +6,7 @@
 /*   By: mtrembla <mtrembla@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 13:10:18 by mtrembla          #+#    #+#             */
-/*   Updated: 2023/02/10 13:58:47 by mtrembla         ###   ########.fr       */
+/*   Updated: 2023/02/13 13:13:21 by mtrembla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 #include <stdio.h>
 #include "../libft/libft.h"
 
+/*
+To use with other type of variable, modify s_node *content
+and both dlist_add prototype.
+*/
 typedef struct s_node
 {
 	char			*content;
@@ -38,7 +42,7 @@ void	dlist_free(t_dlist *l);
 //free list AND content
 void	dlist_free_content(t_dlist *l);
 //remove a node, no free
-void	dlist_remove_node(t_node *node);
+void	dlist_remove_node(t_dlist *l, t_node *node);
 //print list content
 void	view(t_dlist l);
 
--- dlist_remove.c
+++ dlist_remove.c
@@ -6,21 +6,33 @@
 /*   By: mtrembla <mtrembla@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 13:24:43 by mtrembla          #+#    #+#             */
-/*   Updated: 2023/02/10 13:37:24 by mtrembla         ###   ########.fr       */
+/*   Updated: 2023/02/13 13:11:03 by mtrembla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "dlist.h"
 
-void	dlist_remove_node(t_node *node)
+void	dlist_remove_node(t_dlist *l, t_node *node)
 {
+	t_node	*temp;
+
+	temp = node;
 	if (!node->prev && !node->next)
 		return ;
 	else if (!node->prev)
+	{
 		node->next->prev = NULL;
+		l->first = node->next;
+	}
 	else if (!node->next)
+	{
 		node->prev->next = NULL;
+		l->last = node->prev;
+	}
 	else
+	{
 		node->prev->next = node->next;
-	node->next->prev = node->prev;
+		node->next->prev = node->prev;
+	}
+	free(temp);
 }
